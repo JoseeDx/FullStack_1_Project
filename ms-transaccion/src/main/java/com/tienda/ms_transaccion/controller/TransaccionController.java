adb75c5e911d3480d38e7df40b2685b240880384
Catarata17 <ktacorrea23@gmail.com>
2026-05-13 16:10:05 -0400

    ms-transaccion: se agrega logger a controller y liquibase (REVISAR)

--- ms-transaccion/src/main/java/com/tienda/ms_transaccion/controller/TransaccionController.java
+++ ms-transaccion/src/main/java/com/tienda/ms_transaccion/controller/TransaccionController.java
@@ -6,18 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/transacciones")
 public class TransaccionController {
+
+    private static Logger log = LoggerFactory.getLogger(TransaccionController.class);
     
     @Autowired
     private TransaccionService transaccionService;
 
     @GetMapping
     public ResponseEntity<List<Transaccion>> findAll() {
+        log.info("Obteniendo todas las transacciones");
         List<Transaccion> transacciones = transaccionService.findAll();
         if (transacciones.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -27,17 +32,20 @@ public class TransaccionController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Transaccion> findById(@PathVariable Integer id) {
+        log.info("Obteniendo transaccion con ID: {}", id);
         Transaccion transaccion = transaccionService.findById(id);
         return ResponseEntity.ok(transaccion);
     }
 
     @PostMapping
     public ResponseEntity<Transaccion> save(@RequestBody Transaccion transaccion) {
+        log.info("Creando transaccion")
         return new ResponseEntity<>(transaccionService.save(transaccion), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Transaccion> update(@PathVariable Integer id, @RequestBody Transaccion transaccion) {
+        log.info("Actualizando transaccion con ID: {}", id);
         Transaccion existing = transaccionService.findById(id);
         existing.setId_pedido(transaccion.getId_pedido());
         existing.setId_usuario(transaccion.getId_usuario());
@@ -50,6 +58,7 @@ public class TransaccionController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        log.info("Eliminando transaccion con ID: {}", id);
         transaccionService.delete(id);
         return ResponseEntity.noContent().build();
     }
