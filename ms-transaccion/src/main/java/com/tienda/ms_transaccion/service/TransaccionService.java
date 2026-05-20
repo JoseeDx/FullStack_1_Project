7b8ce78cb9917d703bd1de44dfcdc80999bae027
AV-Alumno <AV-Alumno@duoc.acad>
2026-05-20 12:14:08 -0400

    ms-transaccion: se agrega logger a service (REVISAR)

--- ms-transaccion/src/main/java/com/tienda/ms_transaccion/service/TransaccionService.java
+++ ms-transaccion/src/main/java/com/tienda/ms_transaccion/service/TransaccionService.java
@@ -3,6 +3,8 @@ package com.tienda.ms_transaccion.service;
 import com.tienda.ms_transaccion.model.Transaccion;
 import com.tienda.ms_transaccion.repository.TransaccionRepository;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,22 +13,29 @@ import java.util.List;
 @Service
 @Transactional
 public class TransaccionService {
+
+    private static Logger log = LoggerFactory.getLogger(TransaccionService.class);
+
     @Autowired
     private TransaccionRepository transaccionRepository;
     
     public List<Transaccion> findAll() {
+        log.info("Consultando todas las transacciones");
         return transaccionRepository.findAll();
     }
 
     public Transaccion findById(Integer id) {
+        log.info("Obteniendo transaccion por ID");
         return transaccionRepository.findById(id).get();
     }
 
     public Transaccion save(Transaccion transaccion) {
+        log.info("Guardando transaccion");
         return transaccionRepository.save(transaccion);
     }
 
     public void delete(Integer id) {
+        log.info("Borrando transaccion con ID: {}",id);
         transaccionRepository.deleteById(id);
     }
 }
