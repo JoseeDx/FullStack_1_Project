f773efb7ab35429c22089779c09b91e899719e4c
Catalina Correa <ktacorrea23@gmail.com>
2026-05-30 19:12:18 -0400

    ms-transaccion: comentarios exception

--- ms-transaccion/src/main/java/com/tienda/ms_transaccion/exception/BadRequestException.java
+++ ms-transaccion/src/main/java/com/tienda/ms_transaccion/exception/BadRequestException.java
@@ -1,8 +1,10 @@
 package com.tienda.ms_transaccion.exception;
 
 public class BadRequestException extends RuntimeException {
-    public BadRequestException(String mensaje){
-        super(mensaje);
+    public BadRequestException(String mensaje){ 
+        super(mensaje); 
     }
 
+//es igual a resourcenotfound pero cambia el proposito, se lanza cuando el cliente manda 
+// datos incorrectos o invalidos (400 bad request)
 }
--- ms-transaccion/src/main/java/com/tienda/ms_transaccion/exception/ResourceNotFoundException.java
+++ ms-transaccion/src/main/java/com/tienda/ms_transaccion/exception/ResourceNotFoundException.java
@@ -1,8 +1,12 @@
 package com.tienda.ms_transaccion.exception;
 
+//hereda de runtime, es una excepcion no verificada, se lanza cuando no encuentra algo en la base de datos
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String mensaje) {
-        super(mensaje);
+    public ResourceNotFoundException(String mensaje) {//recibe un mensaje y lo pasa a runtime
+        super(mensaje);//llama al constructor de runtime para que el mensaje quede disponible
     }
+
+    //excepcion personalizada, para no lanzar un runtime generico cuando no encuetra algo.
+    // sirve para que devuelva el codigo http correcto (404 not found)
 
 }
